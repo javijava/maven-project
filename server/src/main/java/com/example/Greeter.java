f11b7b09ac479b2dc6dee09cb221c3fd45b964eb
javijava <dejavier78@gmail.com>
2018-10-08 09:31:45 -0300

    subiendo

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,13 +13,10 @@ private String coles = "javiro";
 
   }
 
-public String Greeter(String alguien) 
-
-{
+public String Greeter(String alguien) {
   	return String.format("hola elenote, %s", alguien);
   }
 
-  //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
