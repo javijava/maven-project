8de3bf70c7669abbc2d898efe9b82b0e8adc6356
javijava <dejavier78@gmail.com>
2018-10-08 09:34:19 -0300

    subiendo

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -6,18 +6,16 @@ package com.example;
 public class Greeter {
 
 private String coles = "javiro";
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
 
-  }
+public Greeter() {
+
+}
 
 public String Greeter(String alguien) {
   	return String.format("hola elenote, %s", alguien);
   }
 
-  public String greet(String someone) {
+public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
